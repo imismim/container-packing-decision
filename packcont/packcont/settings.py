51aff11d55cc9c8c4948892f1ed685e27fac3387
Arsen Korytskyi <114431254+imismim@users.noreply.github.com>
2026-03-08 04:32:40 +0200

    refactor: comment out admin-related apps and URL patterns for deployment

--- packcont/packcont/settings.py
+++ packcont/packcont/settings.py
@@ -30,10 +30,10 @@ ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(','
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
+    # 'django.contrib.admin',
+    # 'django.contrib.auth',
+    # 'django.contrib.contenttypes',
+    # 'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     
