1a4049dd7cdc31e3fe51968df17e3578ce4ee0f6
Arsen Korytskyi <114431254+imismim@users.noreply.github.com>
2026-03-08 05:07:14 +0200

    fix: add CSRF_TRUSTED_ORIGINS setting to enhance security

--- packcont/packcont/settings.py
+++ packcont/packcont/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-x4!r0h0**z+rqp0e78e_$
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
-
+CSRF_TRUSTED_ORIGINS = os.environ.get('CSRF_TRUSTED_ORIGINS', ',').split(',')
 
 # Application definition
 
